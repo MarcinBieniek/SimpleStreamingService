4e1e80e5d4d4b29cd4181db16c015d47366e2aab
Marcin Bieniek <marcingim@gmail.com>
2022-06-11 23:43:17 +0200

    Add data agregation form API and home page js code

--- src/js/script.js
+++ src/js/script.js
@@ -68,3 +68,56 @@ const select = {
       }
     });
   }
+
+/* [DONE] 2. Data agregation from API and initing Home content */
+
+const url = settings.db.url + '/' + settings.db.songs;
+
+fetch(url)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(parsedResponse){
+    for(let singleSong of parsedResponse){
+      const songId = singleSong.id;
+      const songTitle = singleSong.title;
+      const songName = singleSong.filename;
+      const songCategories = singleSong.categories;
+      const songRanking = singleSong.ranking;
+      const songFilename = singleSong.filename.toLowerCase();
+      const songCover = singleSong.cover;
+
+      /* Creating correct Author name */
+
+      const authorAndTitle = songFilename.replaceAll('_', ' ').replace('.mp3', '').replace('-', ' ');
+      const songTitleLowerCase = songTitle.toLowerCase();
+      const authorNameLowerCase = authorAndTitle.split(songTitleLowerCase).join('').trim().split(" ");
+      const firstName = authorNameLowerCase[0];
+      const authorNameUpperCase = firstName[0].toUpperCase() + firstName.slice(1);
+      const secondName = authorNameLowerCase[1];
+      const authorSurameUpperCase = secondName[0].toUpperCase() + secondName.slice(1);
+      const authorName = authorNameUpperCase + ' ' + authorSurameUpperCase;
+
+      /* Creating single song HTML object */
+      prepareTemplate(songId, authorName, songTitle, songCategories, songRanking, songName, songCover)
+
+    }
+  })
+  .then(function () {
+    GreenAudioPlayer.init({
+      selector: '.player',
+      stopOthersOnPlay: true
+    });
+  });
+
+/* prepareTemplate function */
+
+let dataArray = [];
+
+const prepareTemplate = function(id, author, title, categories, ranking, songName, cover){
+  const generatedData = {id, author, title, categories, ranking, songName, cover};
+  const pushGeneratedData = templates.player(generatedData);
+document.getElementById(select.containerOf.homeAudioPlayer).innerHTML += pushGeneratedData;
+
+
+dataArray.push(pushGeneratedData)};
